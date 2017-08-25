a711420054888db04a92894c137ddeb870585864
Sinjvf <sinjvf@gmail.com>
2017-08-25 23:30:54 +0300

    teamCity test test push

--- app/src/main/java/com/example/kimichael/yamblz_forecast/App.java
+++ app/src/main/java/com/example/kimichael/yamblz_forecast/App.java
@@ -60,6 +60,8 @@ public class App extends Application {
         } else {
             Timber.plant(new CrashReportingTree());
         }
+
+
     }
 
     private static void setInstance(App instance) {
